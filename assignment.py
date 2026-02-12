41f59c2a4408aa9f2329fbd356c735c6280c9775
crepine-uhiriwe <uhiriwecrepine@gmail.com>
2026-02-12 17:10:23 +0200

    assigmnt

--- assignment.py
+++ assignment.py
@@ -1,21 +1,21 @@
 import sys
-number=[]
+integers=[]
 while True:
     try:
-        userin=input("enter positive integer ( Ctrl+z/Ctrl+d to stop)")
-        num=int(userin)
+        userinput=input("enter positive integer ( Ctrl+z/Ctrl+d to stop)")
+        num=int(userinput)
         if num>0:
-                number.append(num)
+                integers.append(num)
         else:
                 print("ignore",num,"not posovite number")
     except ValueError:
-            print("ignore",userin,"not integer")
+            print("ignore",userinput,"not integer")
     except EOFError:
             print("input stopped")
             break
 if number:
-    print("biggest number", max(number))
-    print("smallest number", min(number))
+    print("biggest number", max(integers))
+    print("smallest number", min(integers))
 else:
     print("no valid positive integer were")
     
